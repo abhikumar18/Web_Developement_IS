420c5055f65d8cd5527e2c8abfa70be238b0c4af
abhikumar18 <abhishek.it.msit@gmail.com>
2023-03-11 10:31:40 +0000

    Category completed

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,11 @@
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Category from './components/categoryList/Category'
 
 function App() {
   return (
     <>
      <Navbar />
      <Hero />
-     <Category />
     </>
   );
 }
